bf25fa8152ab468d3ed3944e18eda1ecb9b9d376
drabelo <http://dailtonrabelo.com>
2015-02-11 14:30:03 -0500

    Version7

--- app/src/main/java/com/example/tmbb/MainActivity.java
+++ app/src/main/java/com/example/tmbb/MainActivity.java
@@ -1,58 +1,77 @@
 package com.example.tmbb;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentTabHost;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 
-import java.util.HashMap;
-import java.util.Stack;
+import com.astuetz.PagerSlidingTabStrip;
 
 public class MainActivity extends ActionBarActivity {
     private static final String TAB_1_TAG = "list";
     private static final String TAB_2_TAG = "measurements";
-    // Fragment TabHost as mTabHost
-    private FragmentTabHost mTabHost;
-    // Tab back stacks
-    private HashMap<TabType, Stack<String>> backStacks;
+    ViewPager mViewPager;
 
-    @Override
+
+
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTabHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
-        mTabHost.setup(this, getSupportFragmentManager(), R.id.realtabcontent);
+        // Get the ViewPager and set it's PagerAdapter so that it can display items
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
 
-
-        // Create tabs
-
-
-        mTabHost.addTab(mTabHost.newTabSpec(TAB_1_TAG).setIndicator("Threads"),
-                ListContainerFragment.class, null);
-        mTabHost.addTab(mTabHost.newTabSpec(TAB_2_TAG).setIndicator("High Scores"),
-                meaFrag.class, null);
-
+        // Give the PagerSlidingTabStrip the ViewPager
+        PagerSlidingTabStrip tabsStrip = (PagerSlidingTabStrip) findViewById(R.id.tabs);
+        // Attach the view pager to the tab strip
+        tabsStrip.setViewPager(viewPager);
     }
 
-    @Override
-    public void onBackPressed() {
-        boolean isPopFragment = false;
-        String currentTabTag = mTabHost.getCurrentTabTag();
-        if (currentTabTag.equals(TAB_1_TAG)) {
-            isPopFragment = ((BaseContainerFragment) getSupportFragmentManager().findFragmentByTag(TAB_1_TAG)).popFragment();
-        } else if (currentTabTag.equals(TAB_2_TAG)) {
-            isPopFragment = ((BaseContainerFragment) getSupportFragmentManager().findFragmentByTag(TAB_2_TAG)).popFragment();
+
+    public static class MyPagerAdapter extends FragmentPagerAdapter {
+        private static int NUM_ITEMS = 2;
+
+        public MyPagerAdapter(FragmentManager fragmentManager) {
+            super(fragmentManager);
         }
-        if (!isPopFragment) {
-            finish();
+
+        // Returns total number of pages
+        @Override
+        public int getCount() {
+            return NUM_ITEMS;
         }
+
+        // Returns the fragment to display for that page
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0: // Fragment # 0 - This will show FirstFragment
+                    return SmsListFragment.newInstance(0, "Threads");
+                case 1: // Fragment # 0 - This will show FirstFragment different title
+                    return ScoresFragment.newInstance(1, "High Scores");
+                default:
+                    return null;
+            }
+        }
+
+        // Returns the page title for the top indicator
+        @Override
+        public CharSequence getPageTitle(int position) {
+            String title = "";
+            if(position == 0)
+            title = "Threads";
+            if(position ==1)
+            title = "High Scores";
+
+            return title;
+        }
+
     }
 
 
-    enum TabType {
-        LIST, MEASUREMENTS
-    }
 
 }
 
